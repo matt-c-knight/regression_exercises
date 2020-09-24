cb5b0acc013654dd600ac95d94e2a9132802a11c
Matthew Knight <mattknight.sa@gmail.com>
2020-09-24 11:43:34 -0500

    updated acquire file

--- acquire.py
+++ acquire.py
@@ -6,3 +6,62 @@ import os
 
 def get_connection(db, user=user, host=host, password=password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+
+################ Acquire Mall Customers Data ###################
+def new_mall_data()
+    '''
+    This function reads the mall customer data from the Codeup db into a df, write
+    it to a csv file, and returns the df.
+    '''
+    sql_query = 'SELECT * FROM customers'
+    df = pd.read_sql(sql_query, get_connection('mall_customers'))
+    df.to_csv('mall_customers_df.csv)')
+    return df
+
+def get_mall_data(cached=False)
+    '''
+    This function reads in mall customer data from Codeup database if cached == False
+    or if cached == True reads in mall customer df from a csv file, returns df
+    '''
+    if cached or os.path.isfile('mall_customers_df.csv') == False:
+        df = new_mall_data()
+    else:
+        df = pd.read_csv('mall_customers_df.csv', index_col=0)
+    return df
+################### 
+def new_titanic_data():
+    sql_query = 'SELECT * FROM passengers'
+    df = pd.read_sql(sql_query, get_connection('titanic_db'))
+    df.to_csv('titanic_df.csv')
+    return df
+
+def get_titanic_data(cached=False):
+    if cached or os.path.isfile('titanic_df.csv') == False:
+        df = new_titanic_data()
+    else:
+        df = pd.read_csv('titanic_df.csv', index_col=0)
+    return df
+
+def new_iris_data():
+    sql_query = """
+                SELECT species_id,
+                species_name,
+                sepal_length,
+                sepal_width,
+                petal_length,
+                petal_width
+                FROM measurements
+                JOIN species
+                USING(species_id)
+                """
+    df = pd.read_sql(sql_query, get_connection('iris_db'))
+    df.to_csv('iris_df.csv')
+    return df
+
+def get_iris_data(cached=False):
+    if cached or os.path.isfile('iris_df.csv') == False:
+        df = new_iris_data()
+    else:
+        df = pd.read_csv('iris_df.csv', index_col=0)
+    return df
--- prepare.py
+++ prepare.py
@@ -7,3 +7,49 @@ from acquire import get_titanic_data
 
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
+
+def iris_split(df):
+
+    train_validate, test = train_test_split(df, test_size=.2,
+                                        random_state=123,
+                                        stratify=df.species)
+    train, validate = train_test_split(train_validate, test_size=.3,
+                                        random_state=123,
+                                        stratify=train_validate.species)
+    return train, validate, test
+
+
+def titanic_split(df):
+
+    train_validate, test = train_test_split(df, test_size=.2,
+                                        random_state=123,
+                                        stratify=df.survived)
+    train, validate = train_test_split(train_validate, test_size=.3,
+                                        random_state=123,
+                                        stratify=train_validate.survived)
+    return train, validate, test
+
+def impute_mean_age(train, validate, test):
+
+    imputer = SimpleImputer(strategy = 'mean')
+
+    train['age'] = imputer.fit_transform(train[['age']])
+
+    validate['age'] = imputer.transform(validate[['age']])
+
+    test['age'] = imputer.transform(test[['age']])
+
+    return train, validate, test
+
+def prep_titanic(cached=True):
+    df = get_titanic_data(cached)
+    df = df[~df.embarked.isnull()]
+    titanic_dummies = pd.get_dummies(df.embarked, drop_first=True)
+    df = pd.concat([df, titanic_dummies], axis=1)
+    df = df.drop(columns='deck')
+
+    train, validate, test = titanic_split(df)
+
+    train, validate, test = impute_mean_age(train, validate, test)
+
+    return train, validate, test
