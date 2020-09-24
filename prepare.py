b765294789b89041467ab1faf67ea54a5054ca9c
Matthew Knight <mattknight.sa@gmail.com>
2020-09-24 11:29:44 -0500

    creating files

---
+++ prepare.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from acquire import get_titanic_data
+
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
